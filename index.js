6ab283dec1f1c250b1d085ddf5c159aeffa41330
RaniaIsmaeil <ranyasmaelgmail.com>
2021-06-14 22:13:05 +0300

    Step 3

--- index.js
+++ index.js
@@ -7,4 +7,11 @@ app.get("/", (req, res)=> {
     res.send("ok");
 });
 
+app.get("/test", (req, res)=> {
+    res.send("status:200, message:ok");
+});
+
+app.get("/time", (req, res)=> {
+    res.send("status:200, message:14:20");
+});
 app.listen(3000);
