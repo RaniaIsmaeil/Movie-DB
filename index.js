8cf8395a7e224fa50b25783ff172e0c72c7f0423
RaniaIsmaeil <ranyasmaelgmail.com>
2021-06-15 00:47:49 +0300

    Step 5

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
     var app = express();
     var port = 3000
 
+    const movies = [
+        { title: 'Jaws', year: 1975, rating: 8 },
+        { title: 'Avatar', year: 2009, rating: 7.8 },
+        { title: 'Brazil', year: 1985, rating: 8 },
+        { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+    ]
+
     app.get("/", (req, res)=> {
     res.send("ok");
     });
@@ -9,6 +16,7 @@
     app.get("/test", (req, res)=> {
     res.send("status:200, message:ok");
     });
+
 
     // app.get("/time", (req, res)=> {
     //     res.send("status:200, message:14:20");
@@ -57,4 +65,24 @@
         })
     }
     });
+
+    app.get("/movies/add", (req, res)=> {
+        res.send();
+    });
+    
+    app.get("/movies/get", (req, res)=> {
+        res.send({
+            status:200, 
+            data: movies
+        });
+    });
+    app.get("/movies/edit", (req, res)=> {
+        res.send();
+    });
+    
+    app.get("/movies/delete", (req, res)=> {
+        res.send();
+    });
+    
+
     app.listen(3000);
